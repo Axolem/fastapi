cca54574c3c6f7437ee2906d224b8a888348a06a
Axole Maranajana <axolemar@gmail.com>
2024-10-24 13:57:46 +0200

    try

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+import io
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+with open("labels.txt", "r") as f:
+    class_names = f.readlines()
+
 
 app = FastAPI()
 
